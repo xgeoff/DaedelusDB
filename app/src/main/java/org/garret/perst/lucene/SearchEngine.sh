adfe47a79360506455306a6a1e1c2696e1e2865d
Jefe <xgeoff@gmail.com>
2025-08-19 21:21:26 -0700

    Remove obsolete scripts from lucene module

--- app/src/main/java/org/garret/perst/lucene/SearchEngine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ x${LUCENE_JAR} == x ] ; then
-   export LUCENE_JAR=/usr/local/lucene-1.4.3/build/lucene-1.5-rc1-dev.jar
-fi
-
-java -Xmx512M -classpath $LUCENE_JAR:. SearchEngine -create
