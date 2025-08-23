01ee180bfb9d091050942bd2ae9486f2a89747da
xgeoff <xgeoff@gmail.com>
2025-08-23 14:18:56 -0700

    Update GraphNode.java

--- app/src/main/java/biz/digitalindustry/db/graph/GraphNode.java
+++ app/src/main/java/biz/digitalindustry/db/graph/GraphNode.java
@@ -4,25 +4,22 @@ package biz.digitalindustry.db.graph;
 import org.garret.perst.*;
 import java.util.*;
 
+import org.garret.perst.*;
+
 public class GraphNode extends Persistent {
     public String id;
     public String label;
     public Link<GraphEdge> outgoing;
     public Link<GraphEdge> incoming;
 
-    public GraphNode() {
-        outgoing = storage.createLink();  // returns LinkImpl
-        incoming = storage.createLink();
+    // no-arg constructor for Perst to instantiate when loading
+    public GraphNode() {}
 
-    }
-
-    public GraphNode(String id, String label) {
-        this();
+    public GraphNode(Storage storage, String id, String label) {
+        super(storage);                   // sets the storage field in superclass
         this.id = id;
         this.label = label;
-    }
-
-    public String toString() {
-        return "Node(" + id + ", " + label + ")";
+        outgoing = storage.createLink();  // returns LinkImpl
+        incoming = storage.createLink();
     }
 }
