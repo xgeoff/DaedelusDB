d0e22827fc8559c8fcacd6fd9459ba7b95ac1b8d
Jefe <xgeoff@gmail.com>
2025-08-24 12:27:17 -0700

    Use ReentrantReadWriteLock for PersistentResource

--- perst-core/src/main/java/org/garret/perst/PersistentResource.java
+++ perst-core/src/main/java/org/garret/perst/PersistentResource.java
@@ -1,149 +1,97 @@
 package org.garret.perst;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * Base class for persistent capable objects supporting locking
  */
 
 public class PersistentResource extends Persistent implements IResource {
-    public synchronized void sharedLock() {
-        Thread currThread = Thread.currentThread();
-        try { 
-            while (true) { 
-                if (owner == currThread) { 
-                    nWriters += 1;
-                    break;
-                } else if (nWriters == 0) { 
-                    if (storage == null || storage.lockObject(this)) { 
-                        nReaders += 1;
-                    }
-                    break;
-                } else { 
-                    wait();
-                }
-            }
-        } catch (InterruptedException x) { 
-            throw new StorageError(StorageError.LOCK_FAILED);
-        }
-    }
-                    
-    public boolean sharedLock(long timeout) {
-        Thread currThread = Thread.currentThread();
-        long startTime = System.currentTimeMillis();
-        synchronized (this) { 
-            try { 
-                while (true) { 
-                    if (owner == currThread) { 
-                        nWriters += 1;
-                        return true;
-                    } else if (nWriters == 0) { 
-                        if (storage == null || storage.lockObject(this)) { 
-                            nReaders += 1;
-                        }
-                        return true;
-                    } else { 
-                        long currTime = System.currentTimeMillis();
-                        if (currTime < startTime) { 
-                            currTime = startTime;
-                        }
-                        if (startTime + timeout <= currTime) { 
-                            return false;
-                        }
-                        wait(startTime + timeout - currTime);
-                    }
-                }
-            } catch (InterruptedException x) { 
-                return false;
-            }
-        }
-    }
-                    
-    public synchronized void exclusiveLock() {
-        Thread currThread = Thread.currentThread();
-        try { 
-            while (true) { 
-                if (owner == currThread) { 
-                    nWriters += 1;
-                    break;
-                } else if (nReaders == 0 && nWriters == 0) { 
-                    nWriters = 1;
-                    owner = currThread;
-                    if (storage != null) { 
-                        storage.lockObject(this);
-                    }
-                    break;
-                } else { 
-                    wait();
-                }
-            }
-        } catch (InterruptedException x) { 
-            throw new StorageError(StorageError.LOCK_FAILED);
-        }
-    }
-                    
-    public boolean exclusiveLock(long timeout) {
-        Thread currThread = Thread.currentThread();
-        long startTime = System.currentTimeMillis();
-        synchronized (this) { 
-            try { 
-                while (true) { 
-                    if (owner == currThread) { 
-                        nWriters += 1;
-                        return true;
-                    } else if (nReaders == 0 && nWriters == 0) { 
-                        nWriters = 1;
-                        owner = currThread;
-                        if (storage != null) { 
-                            storage.lockObject(this);
-                        }
-                        return true;
-                    } else { 
-                        long currTime = System.currentTimeMillis();
-                        if (currTime < startTime) { 
-                            currTime = startTime;
-                        }
-                        if (startTime + timeout <= currTime) { 
-                            return false;
-                        }
-                        wait(startTime + timeout - currTime);
-                    }
-                }
-            } catch (InterruptedException x) { 
-                return false;
-            }
-        }
-    }
-                   
-    public synchronized void unlock() { 
-        if (nWriters != 0) { 
-            if (--nWriters == 0) { 
-                owner = null;
-                notifyAll();
-            }
-        } else if (nReaders != 0) { 
-            if (--nReaders == 0) { 
-                notifyAll();
+    public void sharedLock() {
+        if (lock.isWriteLockedByCurrentThread()) {
+            lock.writeLock().lock();
+        } else {
+            lock.readLock().lock();
+            if (storage != null && lock.getReadLockCount() == 1 && !lock.isWriteLocked()) {
+                storage.lockObject(this);
             }
         }
     }
 
-    public synchronized void reset() { 
-        if (nWriters > 0) { 
-            nWriters = 0;
-            nReaders = 0;
-            owner = null;
-        } else if (nReaders > 0) { 
-            nReaders -= 1;
+    public boolean sharedLock(long timeout) {
+        if (lock.isWriteLockedByCurrentThread()) {
+            lock.writeLock().lock();
+            return true;
         }
-        notifyAll();
+        try {
+            if (lock.readLock().tryLock(timeout, TimeUnit.MILLISECONDS)) {
+                if (storage != null && lock.getReadLockCount() == 1 && !lock.isWriteLocked()) {
+                    storage.lockObject(this);
+                }
+                return true;
+            }
+            return false;
+        } catch (InterruptedException x) {
+            return false;
+        }
+    }
+
+    public void exclusiveLock() {
+        lock.writeLock().lock();
+        if (storage != null && lock.getReadLockCount() == 0 && lock.getWriteHoldCount() == 1) {
+            storage.lockObject(this);
+        }
+    }
+
+    public boolean exclusiveLock(long timeout) {
+        if (lock.isWriteLockedByCurrentThread()) {
+            lock.writeLock().lock();
+            return true;
+        }
+        try {
+            if (lock.writeLock().tryLock(timeout, TimeUnit.MILLISECONDS)) {
+                if (storage != null && lock.getReadLockCount() == 0 && lock.getWriteHoldCount() == 1) {
+                    storage.lockObject(this);
+                }
+                return true;
+            }
+            return false;
+        } catch (InterruptedException x) {
+            return false;
+        }
+    }
+
+    public void unlock() {
+        if (lock.isWriteLockedByCurrentThread()) {
+            lock.writeLock().unlock();
+        } else {
+            lock.readLock().unlock();
+        }
+    }
+
+    public void reset() {
+        while (lock.isWriteLockedByCurrentThread()) {
+            lock.writeLock().unlock();
+        }
+        int n = lock.getReadHoldCount();
+        for (int i = 0; i < n; i++) {
+            lock.readLock().unlock();
+        }
     }
 
     public PersistentResource() {}
-    
-    public PersistentResource(Storage storage) { 
+
+    public PersistentResource(Storage storage) {
         super(storage);
     }
 
-    private transient Thread owner;
-    private transient int    nReaders;
-    private transient int    nWriters;
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        lock = new ReentrantReadWriteLock();
+    }
+
+    private transient ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 }
